8d72707dbaba8bf2df3270a6b1894464b7ee6f6a
Muhammadsaqibkhalid008 <110395786+Muhammadsaqibkhalid008@users.noreply.github.com>
2024-03-07 00:49:44 +0500

    data deleted

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Cinemachine;
 using PowerslideKartPhysics;
 using UnityEngine.UI;
-using TMPro;
+//using TMPro;
 
 public class GameStarter : MonoBehaviour
 {
@@ -12,18 +12,21 @@ public class GameStarter : MonoBehaviour
     [SerializeField] Transform[] allkarts = new Transform[6];
     [SerializeField] UI_Data ui_data;
     [SerializeField] CinemachineVirtualCamera mainVcam;
-    [SerializeField] TMP_Text roundTimeText;
+   // [SerializeField] TMP_Text roundTimeText;
+    public Text RoundText;
     [SerializeField] GameObject timePanel;
     [SerializeField] GameObject gameOverPanel;
-    [SerializeField] TMP_Text totalCoinsCollectedText;
-    [SerializeField] TMP_Text totalEnemiesKilledText;
+    public Text TotalCoinsCollect;
+    public Text TotalEnemeisKilled;
+   // [SerializeField] TMP_Text totalCoinsCollectedText;
+   // [SerializeField] TMP_Text totalEnemiesKilledText;
 
     [Header("this will be the kart starting position")]
     [SerializeField] Transform kartStartingPosition;
 
     [Tooltip("the Ui text mesh pro for total kills count")]
-    [SerializeField] TMP_Text total_kills_ui;
-
+  //  [SerializeField] TMP_Text total_kills_ui;
+    public Text Kills_UI_text;
 
     public Camera mainCamera;
     private bool startGame = false;
@@ -40,7 +43,8 @@ public class GameStarter : MonoBehaviour
     private void Awake()
     {
 
-        this.roundTimeText.gameObject.SetActive(false);
+       // this.roundTimeText.gameObject.SetActive(false);
+        this.RoundText.gameObject.SetActive(false);
         this.gameOverPanel.SetActive(false);
         Time.timeScale = 0;
 
@@ -92,20 +96,24 @@ public class GameStarter : MonoBehaviour
                 minConstant = (int.Parse(minConstant) - 1).ToString();
                 timer = 60f;
             }
-            roundTimeText.text = $"{minConstant}:{(int)timer}";
+            //  roundTimeText.text = $"{minConstant}:{(int)timer}";
+            RoundText.text = $"{minConstant}:{(int)timer}";
         }
 
         if (minConstant == "0" && timer <= 0.10f)
         {
             this.DisplayGameOverPanel();
             // since this is the logic for the game over, we need to update the totalCoinsCollected and the totalEnemiesKilled
-            this.totalCoinsCollectedText.text = "Total coins collected " + this.playerUI.totalCoins.ToString();
-            this.totalEnemiesKilledText.text = "Total enemies killed " + ui_data.totalEnemiesKilled.ToString();
+          //  this.totalCoinsCollectedText.text = "Total coins collected " + this.playerUI.totalCoins.ToString();
+            this.TotalCoinsCollect.text = "Total coins collected " + this.playerUI.totalCoins.ToString();
+           // this.totalEnemiesKilledText.text = "Total enemies killed " + ui_data.totalEnemiesKilled.ToString();
+            this.TotalEnemeisKilled.text = "Total enemies killed " + ui_data.totalEnemiesKilled.ToString();
         }
 
 
         // controlling the ui text mesh pro (total enemy kills)
-        total_kills_ui.text = $"Kills: {ui_data.totalEnemiesKilled.ToString()}";
+        //  total_kills_ui.text = $"Kills: {ui_data.totalEnemiesKilled.ToString()}";
+        Kills_UI_text.text = $"Kills: {ui_data.totalEnemiesKilled.ToString()}";
     }
     public void OnClickTimeButton(int timeIndex = 1)
     {
@@ -116,7 +124,8 @@ public class GameStarter : MonoBehaviour
         Time.timeScale = 1;
         minConstant = timeIndex.ToString();
         this.timePanel.SetActive(false);
-        this.roundTimeText.gameObject.SetActive(true);
+       // this.roundTimeText.gameObject.SetActive(true);
+        this.RoundText.gameObject.SetActive(true);
 
         // making an extra logic for one minute
         minConstant = (timeIndex - 1).ToString();
